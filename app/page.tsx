7764a18f514116d82c7c043ceb96c6438a087fdc
Sam Selikoff <sam.selikoff@gmail.com>
2023-05-27 14:39:21 -0400

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,75 +1,29 @@
 "use client";
 
-import {
-  MotionValue,
-  animate,
-  motion,
-  useMotionTemplate,
-  useMotionValue,
-  useSpring,
-  useTransform,
-} from "framer-motion";
-import { useEffect, useRef, useState } from "react";
+import { MotionValue, motion, useSpring, useTransform } from "framer-motion";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   let [count, setCount] = useState(0);
 
   return (
-    <div className="flex p-20">
-      <div className="w-1/2">
-        <div className="mt-8">
-          <Counter value={count} />
-        </div>
-      </div>
-      <div className="w-1/2">
-        <div className="flex-col">
+    <div className="flex h-full items-center justify-center">
+      <div className="flex w-1/2 justify-center">
+        <div className="flex-col text-center">
           <p>Count: {count}</p>
           <div className="mt-4">
             <input
               type="number"
               value={count}
+              min={0}
               onChange={(e) => setCount(+e.target.value)}
-              name=""
-              id=""
+              className="w-20 p-1"
             />
           </div>
-          {/* <button onClick={() => setCount(0)} className="block w-full border">
-            Set to 0
-          </button>
-          <button onClick={() => setCount(1)} className="block w-full border">
-            Set to 1
-          </button>
-          <button onClick={() => setCount(10)} className="block w-full border">
-            Set to 10
-          </button>
-          <button onClick={() => setCount(20)} className="block w-full border">
-            Set to 20
-          </button>
-          <button onClick={() => setCount(25)} className="block w-full border">
-            Set to 25
-          </button>
-          <button onClick={() => setCount(50)} className="block w-full border">
-            Set to 50
-          </button>
-          <button onClick={() => setCount(99)} className="block w-full border">
-            Set to 99
-          </button>
-          <button
-            onClick={() => setCount(1000)}
-            className="block w-full border"
-          >
-            Set to 1000
-          </button> */}
         </div>
-        {/* <input
-          type="range"
-          value={count}
-          min={0}
-          max={20}
-          onChange={(e) => setCount(+e.target.value)}
-          name=""
-          id=""
-        /> */}
+      </div>
+      <div className="flex w-1/2 items-end justify-center">
+        <Counter value={count} />
       </div>
     </div>
   );
@@ -77,50 +31,57 @@ export default function Home() {
 
 function Counter({ value }: { value: number }) {
   let animatedValue = useSpring(value);
-  let newAnimatedValue = useTransform(animatedValue, (v) => v / 10);
-
   useEffect(() => {
     animatedValue.set(value);
   }, [animatedValue, value]);
 
   return (
-    <div className="flex h-6 w-12 ring-2 ring-red-500">
+    <div className="flex h-6 ring-2 ring-red-500">
       <div className="relative w-6">
-        {[...Array(10).keys()].map((digit) => (
-          <Digit mv={newAnimatedValue} key={digit} digit={digit} />
+        {[...Array(10).keys()].map((i) => (
+          <Number place={100} mv={animatedValue} number={i} key={i} />
         ))}
       </div>
       <div className="relative w-6">
-        {[...Array(10).keys()].map((digit) => (
-          <Digit mv={animatedValue} key={digit} digit={digit} />
+        {[...Array(10).keys()].map((i) => (
+          <Number place={10} mv={animatedValue} number={i} key={i} />
+        ))}
+      </div>
+      <div className="relative w-6">
+        {[...Array(10).keys()].map((i) => (
+          <Number place={1} mv={animatedValue} number={i} key={i} />
         ))}
       </div>
     </div>
   );
 }
 
-function Digit({ mv, digit }: { mv: MotionValue; digit: number }) {
-  let y = useTransform(mv, (v) => {
+function Number({
+  place,
+  mv,
+  number,
+}: {
+  place: number;
+  mv: MotionValue;
+  number: number;
+}) {
+  let y = useTransform(mv, (latest) => {
     let height = 24;
-    let lastDigitOfMv = v % 10;
+    let placeValue = (latest / place) % 10;
+    let offset = (10 + number - placeValue) % 10;
 
-    let offset = (digit - lastDigitOfMv + 10) % 10;
-    let y = offset * height;
+    let memo = offset * height;
 
     if (offset > 5) {
-      y -= 10 * height;
+      memo -= 10 * height;
     }
 
-    return y;
+    return memo;
   });
 
   return (
-    <motion.div
-      style={{ y }}
-      transition={{ ease: "linear", duration: 2 }}
-      className="absolute inset-0 flex justify-center"
-    >
-      <span>{digit}</span>
-    </motion.div>
+    <motion.span style={{ y }} className="absolute inset-0 flex justify-center">
+      {number}
+    </motion.span>
   );
 }
